29e8a60821735dbf23f60be60c53a0024a5a794f
Philipp Kontsarenko <kontsarenko@gmail.com>
2018-07-13 20:36:26 +0300

    Create delete.py

---
+++ delete.py
@@ -0,0 +1,37 @@
+import requests
+import time
+import json
+import yaml
+
+with open("/home/pi/bell/raspberry-bell/config.yml", 'r') as ymlfile:
+    config = yaml.load(ymlfile)
+
+#Delete files older than this:
+ts_to = int(time.time()) - 30 * 24 * 60 * 60
+
+def list_files():
+  params = {
+    'token': token
+    ,'ts_to': ts_to
+    ,'count': 1000
+  }
+  uri = 'https://slack.com/api/files.list'
+  response = requests.get(uri, params=params)
+  return json.loads(response.text)['files']
+
+def delete_files(file_ids):
+  count = 0
+  num_files = len(file_ids)
+  for file_id in file_ids:
+    count = count + 1
+    params = {
+      'token': token
+      ,'file': file_id
+      }
+    uri = 'https://slack.com/api/files.delete'
+    response = requests.get(uri, params=params)
+    print count, "of", num_files, "-", file_id, json.loads(response.text)['ok']
+
+files = list_files()
+file_ids = [f['id'] for f in files]
+delete_files(file_ids)
